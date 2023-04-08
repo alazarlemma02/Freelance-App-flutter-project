891701898fa307fceb167ce629f688da2e199356
FanuelAnte <apexfanuel@gmail.com>
2023-04-09 01:41:16 +0300

    Created the available jobs and ongoing jobs screens.

---
+++ lib/view/screens/ongoing_jobs_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:sira/constants/colors.dart';
+import 'package:sira/view/widgets/job_card_widget.dart';
+
+class OngoingJobs extends StatefulWidget {
+  const OngoingJobs({super.key});
+
+  @override
+  State<OngoingJobs> createState() => _OngoingJobsState();
+}
+
+class _OngoingJobsState extends State<OngoingJobs> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // drawer: Drawer(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => {},
+            icon: Icon(
+              Icons.notifications_outlined,
+              color: CustomColors.blackTextColor,
+            ),
+          ),
+          IconButton(
+            onPressed: () => {},
+            icon: Icon(
+              Icons.person_outline,
+              color: CustomColors.blackTextColor,
+            ),
+          ),
+        ],
+        title: Text("Ongoing Jobs"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+            child: Container(
+              color: CustomColors.cardColor,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+                child: TextField(
+                  style: TextStyle(
+                    color: CustomColors.fadedTextColor,
+                    // fontSize: 16,
+                  ),
+                  decoration: InputDecoration(
+                    isDense: true,
+                    hintText: "Search",
+                    icon: Icon(
+                      Icons.search_outlined,
+                      size: 24,
+                      color: CustomColors.blackTextColor,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+              children: [
+                JobCard(
+                  parentPage: "ongoing jobs",
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
