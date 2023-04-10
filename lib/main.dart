4984996c1869ad5db9841d8da268a667c49c58e5
keniKT <keni232127@gmail.com>
2023-04-10 05:58:31 -0700

    made the main file default

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sira/view/screens/applicant_profile_page.dart';
-import 'package:sira/view/screens/job_application_page.dart';
-import 'package:sira/view/screens/job_detail_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      // debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -26,9 +24,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        // primarySwatch: Colors.blue,
       ),
-      home: const JobApplicationpage (),
+      // home: const JobApplicationpage (),
     );
   }
 }
