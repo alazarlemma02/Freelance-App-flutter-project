b70c83982b261bf9cce37b8d49b332518d8b6d37
Alazar <alazar.lemma@bitscollege.edu.et>
2023-04-18 15:32:46 +0300

    this commit is only to solve merge conflict

--- lib/view/screens/login_page.dart
+++ lib/view/screens/login_page.dart
@@ -163,7 +163,9 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                         ElevatedButton(
                           style: ElevatedButton.styleFrom(
-                              fixedSize: const Size(350, 10),
+                              // fixedSize: const Size(350, 10),
+                              maximumSize:
+                                  Size(MediaQuery.of(context).size.width, 40),
                               backgroundColor: CustomColors.buttonBlueColor),
                           onPressed: _signInUser,
                           child: _isloading
@@ -215,7 +217,9 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                         ElevatedButton(
                           style: ElevatedButton.styleFrom(
-                            fixedSize: const Size(350, 10),
+                            // fixedSize: const Size(350, 10),
+                            minimumSize:
+                                Size(MediaQuery.of(context).size.width, 40),
                             backgroundColor: CustomColors.blackTextColor,
                           ),
                           onPressed: () {
