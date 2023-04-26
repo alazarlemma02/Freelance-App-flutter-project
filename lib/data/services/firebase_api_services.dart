b83f9a2127f132915f3e1cc1ba8deb5aa4cfefcd
keniKT <keni232127@gmail.com>
2023-04-26 13:29:26 -0700

    fetched user profile to my profile

--- lib/data/services/firebase_api_services.dart
+++ lib/data/services/firebase_api_services.dart
@@ -33,4 +33,13 @@ class FirebaseApiServices {
 
     return List.from(jobs.docs.map((doc) => Job.fromSnapshot(doc)));
   }
+
+  Future getUser() async {
+    var currentUser = await FirebaseAuth.instance.currentUser!.uid;
+    var user = await FirebaseFirestore.instance.collection('users').doc(currentUser).get();
+    
+    return UserModel.fromSnapshot(user);
+  }
+
+
 }
