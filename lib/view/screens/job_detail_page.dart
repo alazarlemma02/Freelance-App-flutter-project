b3182f851760b88e81ef8ad683c8795a20274204
FanuelAnte <apexfanuel@gmail.com>
2023-04-27 18:56:14 +0300

    proper display of a given job's details

--- lib/view/screens/job_detail_page.dart
+++ lib/view/screens/job_detail_page.dart
@@ -2,7 +2,13 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sira/bloc/job_bloc_bloc.dart';
 import 'package:sira/constants/colors.dart';
+import 'package:sira/view/screens/posted_jobs_page.dart';
+import 'package:sira/view/widgets/applicant_card.dart';
+import 'package:sira/view/widgets/applicant_count.dart';
+import 'package:sira/view/widgets/job_detail_card.dart';
 
 class JobDetailPage extends StatefulWidget {
   const JobDetailPage({super.key});
@@ -14,71 +20,82 @@ class JobDetailPage extends StatefulWidget {
 class _JobDetailPageState extends State<JobDetailPage> {
   @override
   Widget build(BuildContext context) {
+    final arguments = (ModalRoute.of(context)?.settings.arguments ??
+        <String, dynamic>{}) as Map;
+
     return Scaffold(
       backgroundColor: CustomColors.backgroundColor,
       appBar: AppBar(
         backgroundColor: CustomColors.transparentColor,
         elevation: 0,
-        leading: Icon(Icons.arrow_back,color: CustomColors.blackTextColor,),
-        title: Text('UI Designer',style: TextStyle(color:CustomColors.blackTextColor)),
-      
+        title: Text(
+          'Job Details',
+          style: TextStyle(color: CustomColors.blackTextColor),
+        ),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/PostedJobs');
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
         actions: [
-          IconButton(onPressed: () {
-          }, icon: Icon(Icons.person_outline,color:CustomColors.blackTextColor ,),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.person_outline,
+              color: CustomColors.blackTextColor,
+            ),
           ),
-           IconButton(onPressed: () {
-          }, icon: Icon(Icons.notifications_outlined,color:CustomColors.blackTextColor),
-          )
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.notifications_outlined,
+              color: CustomColors.blackTextColor,
+            ),
+          ),
         ],
       ),
-        body: Padding(
-          padding: const EdgeInsets.fromLTRB(20.0,15 ,20.0,20.0),
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.4,
-            color:CustomColors.cardColor,
-            child: Column(children: [
-              Expanded(
-                flex: 15,
-                child: Container(
-                  // color: Colors.green,
-                  child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(8.0,10.0,8.0,0.0,),
-                    child: Container(child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                      Text("price-range".tr().toString(),style: TextStyle(fontSize: 15),),
-                      Text('Br 2500-400',style: TextStyle(fontSize: 15, color: CustomColors.goldenColor),),
-                    ]),),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(right: 10),
-                    color: CustomColors.fadedTextColor,
-                    width: 80,
-                    height: 30,
-                    child: TextButton(
-                    
-                    child: Text('Design',textAlign: TextAlign.center,style: TextStyle(fontSize: 12, color: CustomColors.blackTextColor),),
-                    onPressed: () {
-                    
-                  },),),
-                ]),),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            JobDetailCard(
+              applicaitionDeadline: arguments['job'].applicationDeadline,
+              category: arguments['job'].category,
+              desctiption: arguments['job'].jobDescription,
+              priceLimit: arguments['job'].priceLimit,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 8,
+                bottom: 8,
               ),
-              Expanded(flex:85,child: Container(
-                padding: EdgeInsets.only(left: 10, right: 10, top: 10),
-                child: Text('We are looking for an experienced UI designer. We are looking for an experienced UI designer. We are looking for an experienced UI designer. We are looking for an experienced UI designer.  '),),),
-            ]),
-
-          ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      "Applicants",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  ApplicantCountWidget(applicantCount: 0)
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children: [
+                  ApplicantCard(),
+                ],
+              ),
+            ),
+          ],
         ),
+      ),
     );
-        
-        
-      
-    
-    
   }
 }
